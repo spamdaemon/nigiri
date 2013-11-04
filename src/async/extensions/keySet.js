b905cc118ca781051272a837534c9eb22ead0eac
spamdaemon <spamdaemon@comast.net>
2013-11-03 19:29:44 -0500

    Enhance keyset functionality with merge and intersect.
    You can intersect and merge two keysets.

--- src/async/extensions/keySet.js
+++ src/async/extensions/keySet.js
@@ -1,7 +1,51 @@
-var MyKeySet = (function(MyFactory, MyKeyRange, findByBinarySearch) {
-    var compareKeys = MyFactory.cmp;
+var MyKeySet = (function(TheFactory, TheKeyRange, findByBinarySearch) {
+    var compareKeys = TheFactory.cmp;
 
-    var MyKeySet = function(keys, verified) {
+    var intersectArrays = function(a, b) {
+        var res = [];
+        var i = 0, j = 0, nA = a.length, nB = b.length, outcome;
+        while (i < nA && j < nB) {
+            outcome = compareKeys(a[i], b[j]);
+            if (outcome === 0) {
+                res.push(a[i]);
+                ++i;
+                ++j;
+            } else if (outcome < 0) {
+                ++i;
+            } else if (outcome > 0) {
+                ++j;
+            }
+        }
+
+        return res;
+    };
+
+    var mergeArrays = function(a, b) {
+        var ai = 0, bi = 0, outcome, val;
+        var res = [];
+        while (ai < a.length && bi < b.length) {
+            outcome = compareKeys(a[ai], b[bi]);
+            if (outcome === 0) {
+                val = a[ai];
+                ++ai;
+                ++bi;
+            } else if (outcome < 0) {
+                val = a[ai++];
+            } else {
+                val = b[bi++];
+            }
+            res.push(val);
+        }
+        while (ai < a.length) {
+            res.push(a[ai++]);
+        }
+        while (bi < b.length) {
+            res.push(b[bi++]);
+        }
+        return res;
+    };
+
+    var TheKeySet = function(keys, verified) {
 
         if (!verified) {
             if (keys.length === 0) {
@@ -15,19 +59,32 @@ var MyKeySet = (function(MyFactory, MyKeyRange, findByBinarySearch) {
         }
 
         this.__keys = Object.freeze(keys.slice(0));
-        this.__range = MyKeyRange.bound(keys[0], keys[keys.length - 1]);
+        this.__range = TheKeyRange.bound(keys[0], keys[keys.length - 1]);
     };
 
-    MyKeySet.prototype.indexOf = function(key) {
-        return findByBinarySearch(this.__keys,compareKeys,0,key);
+    TheKeySet.prototype.indexOf = function(key) {
+        return findByBinarySearch(this.__keys, compareKeys, 0, key);
     };
 
-    MyKeySet.prototype.contains = function(v) {
+    TheKeySet.prototype.contains = function(v) {
         return this.indexOf(v) !== -1;
     };
 
-    addConstProperty(MyKeySet.prototype, "keys");
-    addConstProperty(MyKeySet.prototype, "range");
+    TheKeySet.prototype.intersect = function(ks) {
+        var res = intersectArrays(this.__keys, ks.__keys);
+        if (res.length === 0) {
+            return null;
+        }
+        return new TheKeySet(res, true);
+    };
 
-    return MyKeySet;
+    TheKeySet.prototype.merge = function(ks) {
+        var res = mergeArrays(this.__keys, ks.__keys);
+        return new TheKeySet(res, true);
+    };
+    
+    addConstProperty(TheKeySet.prototype, "keys");
+    addConstProperty(TheKeySet.prototype, "range");
+
+    return TheKeySet;
 })(FACTORY, MyKeyRange, findByBinarySearch);
--- test/spec/async/extensions/keySet.js
+++ test/spec/async/extensions/keySet.js
@@ -1,17 +1,42 @@
 describe("KeyPath", function() {
 
-    it("should find an item in a keyset",function() {
-        
-        var ks = new Nigiri.KeySet([0,1,2,3]);
+    it("should find an item in a keyset", function() {
+
+        var ks = new Nigiri.KeySet([ 0, 1, 2, 3 ]);
         expect(ks.contains(2)).toBe(true);
         expect(ks.indexOf(2)).toBe(2);
     });
 
-    it("should not find an item in a keyset that is not there",function() {
-        
-        var ks = new Nigiri.KeySet([0,1,3]);
+    it("should not find an item in a keyset that is not there", function() {
+
+        var ks = new Nigiri.KeySet([ 0, 1, 3 ]);
         expect(ks.contains(2)).toBe(false);
         expect(ks.indexOf(2)).toBe(-1);
+    });
+
+    it("should intersect two keysets ", function() {
+
+        var ks1 = new Nigiri.KeySet([ 0, 1, 3, 5 ]);
+        var ks2 = new Nigiri.KeySet([ 0, 2, 3, 4, 5 ]);
+        var ks = ks1.intersect(ks2);
+        expect(ks.keys.length).toBe(3);
+        expect(ks.keys[0]).toBe(0);
+        expect(ks.keys[1]).toBe(3);
+        expect(ks.keys[2]).toBe(5);
+    });
+
+    it("should merge two keysets ", function() {
+
+        var ks1 = new Nigiri.KeySet([ 0, 1, 3, 5 ]);
+        var ks2 = new Nigiri.KeySet([ 0, 2, 3, 4, 5 ]);
+        var ks = ks1.merge(ks2);
+        expect(ks.keys.length).toBe(6);
+        expect(ks.keys[0]).toBe(0);
+        expect(ks.keys[1]).toBe(1);
+        expect(ks.keys[2]).toBe(2);
+        expect(ks.keys[3]).toBe(3);
+        expect(ks.keys[4]).toBe(4);
+        expect(ks.keys[5]).toBe(5);
     });
 
 });
