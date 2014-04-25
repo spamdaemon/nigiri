af80c348d013ecd78b326dfc92bf66752769da39
spamdaemon <spamdaemon@comast.net>
2014-04-25 18:54:52 -0400

    Minor update to return an object from database.open

--- src/async/factory.js
+++ src/async/factory.js
@@ -1,4 +1,5 @@
 var FACTORY = (function(REQUEST, INDEXEDDB_PROVIDER) {
+    "use strict";
 
     if (!INDEXEDDB_PROVIDER) {
         INDEXEDDB_PROVIDER = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
@@ -8,19 +9,21 @@ var FACTORY = (function(REQUEST, INDEXEDDB_PROVIDER) {
         throw new Error("Failed to create indexed DB");
     }
 
-    return {
-        open : function() {
+    var provider = function() {
+        this.open = function() {
             var req = INDEXEDDB_PROVIDER.open.apply(INDEXEDDB_PROVIDER, arguments);
             return new REQUEST(req);
-        },
-
-        deleteDatabase : function() {
+        };
+        this.deleteDatabase = function() {
             var req = INDEXEDDB_PROVIDER.deleteDatabase.apply(INDEXEDDB_PROVIDER, arguments);
             return new REQUEST(req);
-        },
+        };
 
-        cmp : function(a, b) {
+        this.cmp = function(a, b) {
             return INDEXEDDB_PROVIDER.cmp(a, b);
-        }
+        };
     };
+
+    return new provider();
+
 })(MyOpenDBRequest);
